36646ba5245f689948ba25387e805f50e155e113
baislsl <baislsl666@gmail.com>
2017-09-02 22:08:06 +0800

    add leetcode head file for c++

---
+++ leetcode/c++/leetcode.h
@@ -0,0 +1,12 @@
+//
+// Created by baislsl on 17-9-2.
+//
+
+#ifndef ALGORITHM_LEETCODE_H
+#define ALGORITHM_LEETCODE_H
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#endif //ALGORITHM_LEETCODE_H
